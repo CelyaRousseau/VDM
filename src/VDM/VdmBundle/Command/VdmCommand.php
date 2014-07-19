7f03778db52b8f6f6bc434f7be0423f736741f03
celya_rousseau <celyarousseau.pro@gmail.com>
2014-07-19 22:51:24 +0200

    [flux_recovery] adding one command to recover RSS stream (from xml) -> adding of author, date of publication and content of the vdm to the database

---
+++ src/VDM/VdmBundle/Command/VdmCommand.php
@@ -0,0 +1,54 @@
+<?php 
+
+
+// src/VDM/VdmBundle/Command/VdmCommand.php
+namespace VDM\VdmBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use VDM\VdmBundle\Entity\Vdm;
+use Doctrine\ORM\EntityManager;
+use \DateTime;
+
+class VdmCommand extends ContainerAwareCommand
+{
+  protected $em;
+
+  protected function configure()
+  {
+    $this
+        ->setName('vdm:flux')
+        ->setDescription('Récupère les dernières Vie de Merde');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $this->em = $this->getContainer()->get('doctrine')->getManager();
+    $this->read("http://feeds.feedburner.com/viedemerde?format=xml");
+  }
+  
+  protected function read($url)
+  { 
+    $xml = simplexml_load_file($url);
+
+    foreach ($xml->entry as $entry) 
+    {
+      $name      = $entry->author->name;
+      $content   = $entry->content;  
+      $published = $entry->published;
+
+      $date = new DateTime($published);      
+      $vdm  = new Vdm();
+
+      $vdm->setAuthor($name);
+      $vdm->setPublished($date);
+      $vdm->setContent($content);
+
+      $this->em->persist($vdm);
+      $this->em->flush();
+    }
+  }
+}
