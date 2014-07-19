f972e0a16da017b61b780334a6745a8cc1d94b93
celya_rousseau <celyarousseau.pro@gmail.com>
2014-07-19 22:46:58 +0200

    [flux_recovery] Adding an entity called Vdm

---
+++ src/VDM/VdmBundle/Entity/Vdm.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace VDM\VdmBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Vdm
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="VDM\VdmBundle\Entity\VdmRepository")
+ */
+class Vdm
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="Published", type="datetime")
+     */
+    private $published;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Content", type="string", length=255)
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Vdm
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set published
+     *
+     * @param \DateTime $published
+     * @return Vdm
+     */
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    /**
+     * Get published
+     *
+     * @return \DateTime 
+     */
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Vdm
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
