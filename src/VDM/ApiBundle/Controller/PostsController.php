2d72ae1e53fe0149ce0dda8e333a13ed80f979f5
celya_rousseau <celyarousseau.pro@gmail.com>
2014-07-21 01:22:54 +0200

    [Api] implementation of getPostsAction() and getPostAction()

--- src/VDM/ApiBundle/Controller/PostsController.php
+++ src/VDM/ApiBundle/Controller/PostsController.php
@@ -2,20 +2,42 @@
 
 namespace VDM\ApiBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class PostsController extends Controller
+use FOS\RestBundle\Controller\FOSRestController,
+    FOS\RestBundle\View\View,
+    FOS\RestBundle\Controller\Annotations\QueryParam,
+    FOS\RestBundle\Request\ParamFetcherInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+class PostsController extends FOSRestController
 {
 
     public function getPostsAction(){
 
-        $vdm = $em->getRepository('ApiBundle:Vdm')->findAll();
-        return $this->render('ApiBundle:Default:index.html.twig', array('vdm' => $vdm));
+        $em   = $this->getDoctrine()->getManager();
+        $data = $em->getRepository('ApiBundle:Vdm')->findAll();
+
+        // $posts = array();
+
+        // foreach($data as $entry)
+        // {
+        //   $post_serialized = json_decode($entry,true);
+        //   array_push($posts, $post_serialized);
+        // }
+
+        // $view = $this->view($data, 200)
+                     // ->setFormat('jsonp');
+
+        // return $this->handleView($view);
+
+        return $data;
+        
     }
 
     public function getPostAction($id){
-
-        $vdm = $em->getRepository('ApiBundle:Vdm')->find($id);
-        return $this->render('ApiBundle:Default:index.html.twig', array('vdm' => $vdm));
+        $em = $this->getDoctrine()->getManager();
+        return $em->getRepository('ApiBundle:Vdm')->find($id);
+        
     }
 }
